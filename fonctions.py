ccb2b491a14467183e794a6bf243ea9157d483ed
thomas <psychoid@hotmail.fr>
2020-05-26 11:07:34 +0200

    renomage et orga des fichies

--- fonctions.py
+++ fonctions.py
@@ -8,40 +8,23 @@ import mysql.connector
 import requests
 import json
 
+import classes
+
 
 ################################################################################
-
-def create_data_base():
-
-    cursor = Connection.connect()
-    
-
-################################################################################
-
-
 
 # This fonction is quite simple, it's allow us to show all the tables we got for 
 # exemple here we just have two tables 'Corn_flakes' and 'Pizza'.
 
 def show_tables():
 
-    connect = mysql.connector.connect(host="localhost",user="student",
-            password="Metalspirit77+", database="aliments")
-    cursor = connect.cursor()
     cursor.execute("""SHOW TABLES""")
     result = cursor.fetchall()
     choice1 = str(result[0]).strip("()',")
     choice2 = str(result[1]).strip("()',")
     print("1  "+choice1+"\n2  "+choice2)
-    connect.close()
 
     return
-
-################################################################################
-
-
-
-
 
 ################################################################################
 
@@ -54,12 +37,11 @@ def healthier_one(nut_score, table):
     nutriscore_level = ['a', 'b', 'c', 'd']
 
     for el in nutriscore_level:
+
         if el == nut_score[1]:
             break
+
         else:
-            connect = mysql.connector.connect(host="localhost",user="student",
-                password="Metalspirit77+", database="aliments")
-            cursor = connect.cursor()
             cursor.execute("""SELECT id, marque, nom, nutriscore, magasin, url 
             FROM """+table+""" WHERE nutriscore = '"""+el+"""' """)
             test = cursor.fetchmany()
@@ -78,16 +60,12 @@ def healthier_one(nut_score, table):
 def save_food(product_num, table):
 
     product_num = str(product_num)
-    connect = mysql.connector.connect(host="localhost",user="student",
-        password="Metalspirit77+", database="aliments")
-    cursor = connect.cursor()
+    
     cursor.execute("""SELECT * FROM """+table+""" WHERE id = """+product_num+""" """)
     result = cursor.fetchone()
     result = str(result)
     cursor.execute("""INSERT INTO Save_food(foreign_key, nom, marque, magasin, 
     pays, quantite, nutriscore, url, categorie) Values"""+result+""" """)
-    connect.commit()
-    connect.close()
 
     return
 
@@ -95,16 +73,12 @@ def save_food(product_num, table):
 
 def show_saved_food():
 
-    connect = mysql.connector.connect(host="localhost",user="student",
-            password="Metalspirit77+", database="aliments")
-    cursor = connect.cursor()
     cursor.execute("""SELECT categorie, nutriscore, marque, nom, magasin, url, 
     foreign_key FROM Save_food ORDER BY categorie""")
     rows = cursor.fetchall()
     for value in rows:
         print("\n[{0}, {1}, {2}, {3}, {4}, {5}, {6}]".format(value[0], value[1], 
         value[2], value[3], value[4], value[5], value[6]))
-    connect.close()
 
     return
 
